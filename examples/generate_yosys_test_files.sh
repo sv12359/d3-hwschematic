5688a4ab6b6257100aa3853b5961afbf61f87e48
sv12359 <s.vadovic9@gmail.com>
2022-08-03 22:36:27 +0200

    yosys: refactor yosys bash scripts

--- examples/generate_yosys_test_files.sh
+++ examples/generate_yosys_test_files.sh
@@ -2,12 +2,19 @@
 
 # Internal scipt for generating yosys test files,
 
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+NC="\033[0m"
 cd verilog
 for filename in *; do
 	name=`echo $filename | cut -d"." -f1`
-	../verilog_to_json.sh $name
+	../verilog_to_json.sh -t $name $filename
+	if test $? -eq 1; then
+		echo -e "\n\n${RED}[Error]${NC} in the translation of verilog file to json: $filename"
+		exit 1
+	fi; 
+	mv $name.json ../schemes_yosys
+
 done;
-
-
-mv *.json ../schemes_yosys
 cd ..
+echo -e "\n\n${GREEN}[Succes]${NC} all veriog files were translated to json"
--- examples/verilog_to_json.sh
+++ examples/verilog_to_json.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
-# Script expects one file with a module of the same name.
-# It creates json form verilog file using yosys.
+# Script expects one top file and verilog files.
+# It creates json from the files using yosys.
+help_message="Usage: $0 -t TOP FILE...
+TOP is top component in verilog to generate json for.
+FILE is a verilog input source file. Multiple file input is supported.
 
-if !(test 1 -eq $#); then
-	echo 1 argument required
-	exit 1
-fi;
+The script generates json from verilog files using yosys."
 
-file=$1
-yosys <<EOF
-read_verilog $file.v;
-hierarchy -top $file;
-synth -run coarse -noabc -noalumacc -nordff;
-splice
-# show -stretch; # show circuit in yosys graphviz
-write_json $file.json;
-EOF
+while getopts "t:h" arg; do
+  case $arg in
+    h)
+      echo "use: $0 -t top verilog_files.v"
+      exit 0;
+      ;;
+    t)
+      top=$OPTARG
+      if test -z top; then
+      	echo top is missing
+      	exit 1
+      fi;
+      ;;
+     ?)
+       exit 1;
+       ;;
+  esac
+  shift;
+  shift;
+  files=""
+  while test -n "$1"; do
+     files="$files $1"
+  	 shift
+  done;
+done
+
+# -noabc is used to avoid the dissolving of operators to elementary gates
+# -noalumacc is used to keep comparators and arithmetic operators in original form
+# -nordff is used to avoid the use of special DFFs
+yosys -Q -p "read_verilog $files;"\
+ -p "hierarchy -top $top;"\
+ -p "synth -run coarse -noabc -noalumacc -nordff;"\
+ -p "splice"\
+ -p "#show -stretch; # show circuit in yosys graphviz"\
+ -p "write_json $top.json;"
+ 
+exit $?
